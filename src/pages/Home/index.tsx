b7f26bd847c25bd4d5944c0265a3edd19e003e6f
Carlos Alexandre <alexfilho062005@gmail.com>
2024-12-27 18:59:47 -0300

    :sparkles: feat: card post implemeten  and format date

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,16 @@ import { useContext } from "react";
 import { CardProfile } from "./components/card-profile";
 import { ProfileContext } from "../../context/ProfileContext";
 import { Input } from "../../components/ui/Input";
+import { CardIssues } from "../../components/ui/CardIssues";
+import { formatDistanceToNowStrict } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 export function Home() {
   const { issuesData } = useContext(ProfileContext);
   return (
-    <main className="mx-auto max-w-wrapped px-3 lg:px-0">
+    <main className="mx-auto max-w-wrapped px-3 pb-6 lg:px-0">
       <CardProfile />
+
       <div className="mt-14 flex items-center justify-between">
         <h3 className="text-lg font-bold leading-[1.6] text-base-subtitle">
           Publicações
@@ -16,8 +20,23 @@ export function Home() {
           {issuesData.length} publicações
         </span>
       </div>
-      <div className="mt-4">
+
+      <form className="mt-4">
         <Input />
+      </form>
+
+      <div className="mt-6 grid gap-6 md:grid-cols-2">
+        {issuesData.map((issue) => (
+          <CardIssues
+            key={issue.id}
+            title={issue.title}
+            created_at={formatDistanceToNowStrict(new Date(issue.created_at), {
+              addSuffix: true,
+              locale: ptBR,
+            })}
+            body={issue.body.substring(0, 200) + "..."}
+          />
+        ))}
       </div>
     </main>
   );
